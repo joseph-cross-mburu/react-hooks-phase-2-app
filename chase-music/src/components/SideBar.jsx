bc8d2eb93039f19f1fff91a5c15ac4a2497cf1fe
cross <crossjay959@gmail.com>
2024-05-09 00:44:09 -0700

    side bar

--- chase-music/src/components/SideBar.jsx
+++ chase-music/src/components/SideBar.jsx
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const Sidebar = () => {
-//   return (
-//     <div className='sidebar'>
-//       <ul>
-//         <li><Link to="/">Dashboard</Link></li>
-//         <li><Link to='./components/About'>About</Link></li>
-//         <li><Link to='./components/ContactUs'>COntactus</Link></li>
-//         <li><Link to='./components/Register'>COntactus</Link></li>
-//       </ul>
-//     </div>
-//   );
-// }
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <ul>
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to='./components/About'>About</Link></li>
+        <li><Link to='./components/ContactUs'>COntactus</Link></li>
+        <li><Link to='./components/Register'>COntactus</Link></li>
+      </ul>
+    </div>
+  );
+}
 
-// export default Sidebar;
+export default Sidebar;
